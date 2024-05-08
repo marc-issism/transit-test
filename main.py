a8214a3f7ac1f2bcf9f3f80fcb7db6a34f6abae9
Marcus Cua <moojycua@gmail.com>
2024-05-08 16:58:14 -0400

    Added XML feed documentation link

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from Vehicle import Vehicle
 # TTC vehicle data: https://webservices.umoiq.com/service/publicXMLFeed?command=vehicleLocations&a=ttc
 # TTC stop data: https://webservices.umoiq.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=905
 # TTC route data: https://webservices.umoiq.com/service/publicXMLFeed?command=routeList&a=ttc
+# Public XML Feed documentation: https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/8217e43a-bba2-4e6c-82f5-bf6d1a52545d/resource/84572a3e-8537-4793-8a92-61107bab85f4/download/NextBusXMLFeed.pdf
 
 if __name__ == '__main__':
     url = "https://webservices.umoiq.com/service/publicXMLFeed?command=vehicleLocations&a=ttc"
